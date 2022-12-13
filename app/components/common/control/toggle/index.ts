581f1d33d92fd4d5faf113ee4076560ef18e6db3
Spuxx <ameisenfutter@outlook.com>
2022-12-13 13:32:02 +0100

    implemented toggle component

---
+++ app/components/common/control/toggle/index.ts
@@ -0,0 +1,50 @@
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import { EmberChangeset } from 'ember-changeset';
+
+export interface Args {
+  label: string;
+  state?: boolean;
+  changeset?: EmberChangeset;
+  key?: string;
+  onChange?: Function;
+  disabled?: boolean;
+}
+
+export default class ToggleComponent extends Component<Args> {
+  declare args: Args;
+
+  constructor(owner: unknown, args: Args) {
+    super(owner, args);
+    if (args.state === undefined && (!args.changeset || !args.key)) {
+      throw new Error(
+        'The Toggle component expects either a state or a changeset-key-pair, but it received neither.'
+      );
+    }
+  }
+
+  get disabled() {
+    return this.args.disabled;
+  }
+
+  get state() {
+    if (this.args.changeset) {
+      return this.args.changeset[this.args.key as string];
+    } else {
+      return this.args.state;
+    }
+  }
+
+  @action handleChange(event: any) {
+    if (this.args.changeset) {
+      this.args.changeset[this.args.key as string] =
+        !this.args.changeset[this.args.key as string];
+      if (this.args.changeset.isValid) {
+        this.args.changeset.save();
+      }
+    }
+    if (this.args.onChange) {
+      this.args.onChange(event);
+    }
+  }
+}
