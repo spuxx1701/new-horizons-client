5874864bffd21f8d49fdf2030a9509c06a266aba
Spuxx <ameisenfutter@outlook.com>
2022-12-23 17:12:35 +0100

    fixed generator sidebar buttons

--- app/components/application/sidebar/left/generator/index.ts
+++ app/components/application/sidebar/left/generator/index.ts
@@ -5,11 +5,11 @@ import GeneratorService from 'new-horizons-client/services/generator';
 export default class LeftSidebarGeneratorComponent extends Component {
   @service declare generator: GeneratorService;
 
-  get originButtonDisabled() {
-    return this.generator.state === 'preset';
+  get originButtonVisible() {
+    return this.generator.state !== 'preset';
   }
 
-  get postOriginButtonDisabled() {
-    return this.generator.state !== 'post-origin';
+  get postOriginButtonsVisible() {
+    return this.generator.state === 'post-origin';
   }
 }
